d7d1f04c7c6d92c96048882be640655d8b64caf2
Josue Jhonatas Catazans Pereira <josue.pereira@duosystem.com.br>
2021-12-01 21:00:15 -0300

    feature: create context

---
+++ src/context/MoviesContext.tsx
@@ -0,0 +1,20 @@
+import {creatContext} from 'react';
+
+import { api } from '../services/api';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
